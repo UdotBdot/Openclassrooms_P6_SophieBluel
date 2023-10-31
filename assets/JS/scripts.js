eb452906183b04f2e3d7787cf59d2e61fc0852b5
UdotBdot <Frederic.sin@proton.me>
2023-10-31 19:38:04 +0100

    REFACTO

--- assets/JS/scripts.js
+++ assets/JS/scripts.js
@@ -1,7 +1,24 @@
-// Constante pour l'URL de l'API
+// Déclaration des constantes et variables pour les éléments DOM et les données
 const apiUrl = "http://localhost:5678/api/works";
 const galleryEl = document.querySelector(".gallery");
+const modalContainer = document.querySelector(".modal-container");
+const modalTriggers = document.querySelectorAll(".modal-trigger");
+const overlay = document.querySelector(".overlay");
+const openModal = document.getElementById("open-modal");
+const allEl = document.getElementById("all");
+const objEl = document.getElementById("obj");
+const appartEl = document.getElementById("appart");
+const hotRestauEl = document.getElementById("hot-restau");
+const addPictureBtnModal1 = document.querySelector(".addpic-btn-modal1");
+const modal2 = document.querySelector(".modal2");
+const previousModalIcon = document.querySelector(".previous-modal");
+const photoInput = document.getElementById("photo");
+const labelImg = document.querySelector('.label-img');
+const titleInput = document.querySelector(".form-title input");
+const optionsSelect = document.querySelector(".form-options select");
+const btnModal2 = document.getElementById("button-form");
 let data = [];
+const tokenSession = sessionStorage.getItem("Token");
 
 // Fonction asynchrone pour récupérer les données depuis l'API
 async function fetchData() {
@@ -15,77 +32,37 @@ async function fetchData() {
   }
 }
 
-// Fonction pour afficher les données dans la galerie
+// Fonction pour afficher les données dans la galerie principale
 function renderData(data) {
   galleryEl.innerHTML = "";
   data.forEach(item => {
+    // Création des éléments pour chaque élément de données
     const newFigure = document.createElement("figure");
     const newImg = document.createElement("img");
     newImg.src = item.imageUrl;
     const newFigCaption = document.createElement("figcaption");
     newFigCaption.textContent = item.title;
+
+    // Construction de la structure de la galerie principale
     newFigure.appendChild(newImg);
     newFigure.appendChild(newFigCaption);
     galleryEl.appendChild(newFigure);
   });
 }
 
-// Écouteurs d'événements pour les filtres de la galerie
-const allEl = document.getElementById("all");
-const objEl = document.getElementById("obj");
-const appartEl = document.getElementById("appart");
-const hotRestauEl = document.getElementById("hot-restau");
-
-allEl.addEventListener('click', () => {
-  renderData(data);
-});
-
-objEl.addEventListener('click', () => {
-  const filteredData = data.filter(item => item.category.name === "Objets");
+// Filtrer les données par catégorie
+function filterData(category) {
+  const filteredData = data.filter(item => item.category.name === category);
   renderData(filteredData);
-});
-
-appartEl.addEventListener('click', () => {
-  const filteredData = data.filter(item => item.category.name === "Appartements");
-  renderData(filteredData);
-});
-
-hotRestauEl.addEventListener('click', () => {
-  const filteredData = data.filter(item => item.category.name === "Hotels & restaurants");
-  renderData(filteredData);
-});
-
-// Écouteur d'événement pour le chargement du document
-document.addEventListener("DOMContentLoaded", () => {
-  fetchData();
-});
-
-// Modale - Ouverture et Fermeture
-const modalContainer = document.querySelector(".modal-container");
-const modalTriggers = document.querySelectorAll(".modal-trigger");
-const openModal = document.getElementById("open-modal");
-const overlay = document.querySelector(".overlay")
-
-modalTriggers.forEach(trigger => trigger.addEventListener("click", toggleModal));
-
-function toggleModal() {
-  modalContainer.style.display = "none"; // Fermer la modale
-  overlay.style.display = "none";
-  modal2.style.display = "none";
-  resetModal2()
 }
 
-openModal.addEventListener("click", function () {
-  modalContainer.style.display = "block"; // Ouvrir la modale
-  overlay.style.display = "block"
-});
-
-// Ajout dynamique des photos dans la modale
+// Créer la galerie dans la modale
 function createGallery(data) {
   const modalGallery = document.querySelector(".modal-gallery");
-  modalGallery.innerHTML = ""; // Effacer le contenu existant
+  modalGallery.innerHTML = "";
 
   data.forEach((item) => {
+    // Création des éléments pour la galerie modale
     const figure = document.createElement("figure");
     figure.innerHTML = `
       <div class="trash" id="${item.id}">
@@ -95,122 +72,121 @@ function createGallery(data) {
     `;
     modalGallery.appendChild(figure);
   });
+
   const trashIcons = document.querySelectorAll(".trash");
   trashIcons.forEach(trashIcon => {
-  trashIcon.addEventListener("click", () => {
-    console.log("J'ai cliqué");
-     // Ajoutez ici le code à la place du console.log pour supprimer l'élément associé à l'icône trash.
+    trashIcon.addEventListener("click", () => {
+      // Action à effectuer lors du clic sur l'icône de suppression
+      console.log("J'ai cliqué");
+      // Ajouter ici le code pour supprimer l'élément associé à l'icône trash.
+    });
   });
-});
 }
 
-// Gestion de la session utilisateur
-const tokenSession = sessionStorage.getItem("Token");
-const refreshPageAdmin = (tokenSession) => {
-  if (tokenSession !== null) {
+// Fonction pour masquer la modale
+function toggleModal() {
+  modalContainer.style.display = "none";
+  overlay.style.display = "none";
+  modal2.style.display = "none";
+  resetModal2();
+}
+
+// Ajout des écouteurs d'événements pour ouvrir et fermer la modale
+modalTriggers.forEach(trigger => trigger.addEventListener("click", toggleModal));
+openModal.addEventListener("click", () => {
+  modalContainer.style.display = "block";
+  overlay.style.display = "block";
+});
+
+// Écouteurs d'événements pour les filtres de la galerie principale
+allEl.addEventListener('click', () => renderData(data));
+objEl.addEventListener('click', () => filterData("Objets"));
+appartEl.addEventListener('click', () => filterData("Appartements"));
+hotRestauEl.addEventListener('click', () => filterData("Hotels & restaurants"));
+
+// Fonction pour rafraîchir la page admin
+function refreshPageAdmin(token) {
+  if (token !== null) {
     const editEl = document.querySelector(".edit");
     editEl.style.display = "block";
     const loginEl = document.getElementById("login");
     loginEl.innerHTML = "logout";
     loginEl.addEventListener("click", () => {
       sessionStorage.removeItem("Token");
-      window.location.href = "/index.html"; // Redirection vers la page des projets après la deconnexion
+      window.location.href = "/index.html";
     });
   }
-};
+}
 
+// Initialisation de la page admin et gestion de la session utilisateur
 refreshPageAdmin(tokenSession);
 
-// Ouverture de la modale 2 
-const addPictureBtnModal1 = document.querySelector(".addpic-btn-modal1");
-const modal2 = document.querySelector(".modal2")
-
-addPictureBtnModal1.addEventListener("click", function(){
+// Ajout des écouteurs d'événements pour la deuxième modale
+addPictureBtnModal1.addEventListener("click", () => {
   modal2.style.display = "block";
-})
-
-// Revenir à la modale 1 à partir de la modale 2 
-
-const previousModalIcon = document.querySelector(".previous-modal");
-
-previousModalIcon.addEventListener("click", function(){
-  modalContainer.style.display = "block";
-  modal2.style.display ="none";
-  resetModal2()
-})
-
-// Fonctionnalités de la 2eme modale
-
-// Écouteur d'événement pour le changement de la photo sélectionnée
-const photoInput = document.getElementById("photo");
-const labelImg = document.querySelector('.label-img'); // Sélection de l'élément avec la classe label-img
-
-photoInput.addEventListener('change', function() {
-    const selectedFile = photoInput.files[0]; // Récupération du fichier sélectionné
-    const fileRegex = /\.(jpe?g|png)$/i; // Expression régulière pour vérifier l'extension
-
-    // Vérification du fichier sélectionné
-    if (selectedFile && fileRegex.test(selectedFile.name)) {
-        const reader = new FileReader();
-
-        reader.onload = function(e) {
-            // Création d'une balise img pour afficher l'image sélectionnée
-            const imageElement = document.createElement('img');
-            imageElement.src = e.target.result;
-            imageElement.alt = 'Selected Image';
-
-            // Effacer le contenu existant de label-img et insérer l'image
-            labelImg.innerHTML = '';
-            labelImg.appendChild(imageElement);
-        };
-
-        // Lecture du contenu du fichier sélectionné
-        reader.readAsDataURL(selectedFile);
-    }
 });
 
-// Fonction pour réinitialiser la deuxième modale
-// Sauvegarde du contenu par défaut au chargement initial
+previousModalIcon.addEventListener("click", () => {
+  modalContainer.style.display = "block";
+  modal2.style.display ="none";
+  resetModal2();
+});
+
+// Écouteur d'événement pour le changement de la photo sélectionnée
+photoInput.addEventListener('change', function() {
+  const selectedFile = photoInput.files[0];
+  const fileRegex = /\.(jpe?g|png)$/i;
+
+  if (selectedFile && fileRegex.test(selectedFile.name)) {
+    const reader = new FileReader();
+
+    reader.onload = function(e) {
+      const imageElement = document.createElement('img');
+      imageElement.src = e.target.result;
+      imageElement.alt = 'Selected Image';
+
+      labelImg.innerHTML = '';
+      labelImg.appendChild(imageElement);
+    };
+
+    reader.readAsDataURL(selectedFile);
+  }
+});
+
+// Réinitialisation de la deuxième modale
 document.addEventListener('DOMContentLoaded', function() {
-  const labelImg = document.querySelector('.label-img');
   labelImg.defaultContent = labelImg.innerHTML;
 });
 
-// Fonction pour réinitialiser la deuxième modal
+// Fonction pour réinitialiser la deuxième modale
 function resetModal2() {
-  const labelImg = document.querySelector('.label-img');
   const defaultContent = labelImg.defaultContent;
-
-  // Supprimer l'image actuelle, si présente
   const imageInsideLabelImg = document.querySelector('.label-img img');
   if (imageInsideLabelImg) {
     imageInsideLabelImg.remove();
-}
+  }
 
-  // Réinsérer le contenu par défaut
   labelImg.innerHTML = defaultContent;
   titleInput.value = "";
   optionsSelect.value = "";
-
-  btnModal2.style.background = "#A7A7A7";
+  changeBtnColor();
 }
 
-// Changer la couleur du bouton 
-const titleInput = document.querySelector(".form-title input");
-const optionsSelect = document.querySelector(".form-options select");
-const btnModal2 = document.getElementById("button-form")
-
-// Fonction pour vérifier et changer la couleur du bouton
+// Fonction pour changer la couleur du bouton en fonction des champs remplis
 function changeBtnColor() {
   if (titleInput.value !== "" && optionsSelect.value !== "" && photoInput.files.length > 0) {
     btnModal2.style.background = "#1D6154";
   } else {
-    btnModal2.style.background = "#A7A7A7"; // Réinitialise la couleur du bouton
+    btnModal2.style.background = "#A7A7A7";
   }
 }
+
 // Ajout d'écouteurs d'événements pour les changements dans les champs du formulaire
 titleInput.addEventListener('input', changeBtnColor);
 optionsSelect.addEventListener('change', changeBtnColor);
 photoInput.addEventListener('change', changeBtnColor);
 
-// Ajout des photos dans la page d'accueil 
+// Événement de chargement de la page pour initialiser le contenu
+document.addEventListener("DOMContentLoaded", () => {
+  fetchData();
+});
